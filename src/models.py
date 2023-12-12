bf069f7876e0909cbda87f04b76b4214e583bf8e
Rofhiwa (Ralph) Matumba <rofhima13@gmail.com>
2023-12-12 13:29:38 +0200

    Refactor for Dr Netshikweta

--- src/models.py
+++ src/models.py
@@ -1,12 +1,12 @@
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adagrad
+from tensorflow.keras.optimizers import *
 import numpy as np
 
 def LSTM_model(input_shape):
     model = Sequential()
-    model.add(LSTM(units=1500, input_shape=input_shape))
+    model.add(LSTM(units=500, input_shape=input_shape))
     model.add(Dense(units=5))
-    model.compile(optimizer=Adagrad(), loss='mse')
+    model.compile(optimizer=SGD(), loss='mse')
 
     return model
